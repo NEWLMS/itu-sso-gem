aea1bfbc08e0297bd9d902770c0e51a3d6d124ec
Willian Fernandes <willian@willianfernandes.com.br>
2014-06-05 04:10:35 +0000

    Fixed current_user helper

--- lib/itu/sso/helpers.rb
+++ lib/itu/sso/helpers.rb
@@ -3,11 +3,15 @@ module ITU
     module Helpers
       def current_user
         unless @current_user
-          client   = Client.new(user_auth_token)
-          sso_user = client.user.get
+          if user_auth_token.present?
+            client   = Client.new(user_auth_token)
+            sso_user = client.user.get
 
-          if sso_user.authentication_token == user_auth_token
-            @current_user = User.find_by(authentication_token: user_auth_token)
+            if sso_user.authentication_token == user_auth_token
+              @current_user = User.find_by(authentication_token: user_auth_token)
+            end
+          else
+            @current_user = nil
           end
         end
 
@@ -23,7 +27,7 @@ module ITU
       protected
 
       def user_auth_token
-        unless @user_auth_token
+        if @user_auth_token.blank? and cookies[:user_auth_token].present?
           key = ActiveSupport::KeyGenerator.new(ENV.fetch('SSO_SECRET_TOKEN')).generate_key(ENV.fetch('SSO_SECRET_SALT'))
           encryptor = ActiveSupport::MessageEncryptor.new(key)
           @user_auth_token = encryptor.decrypt_and_verify(cookies[:user_auth_token])
