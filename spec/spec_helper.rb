caf89a7e23ae1db2f2331418f25da77fa5ceef6a
Willian Fernandes <willian@willianfernandes.com.br>
2014-06-05 01:59:24 +0000

    Removed rspec warning messages.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.dirname(__FILE__) + '/support/config/boot'
 require 'rspec/rails'
-require 'rspec/autorun'
 
 require 'faraday'
 
@@ -13,7 +12,6 @@ VCR.configure do |config|
 end
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
